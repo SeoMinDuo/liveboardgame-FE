781e57618aa47a408408c8353040698ec9877a14
angrymusic <angrymusic@naver.com>
2023-07-20 00:13:03 +0900

    비로그인시 컴포넌트 미 출력

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -16,14 +16,20 @@ function MainPage() {
     }, [login, navigate]);
 
     return (
-        <div className="bg-myWhite h-screen flex justify-center items-center flex-col">
-            <div className="w-1/4 aspect-square bg-gray-900 rounded-3xl">{login.loginInfo.id}</div>
-            <button
-                className="rounded-full active:bg-sky-600 bg-sky-500 hover:bg-sky-400 py-1 mt-5 w-1/5 h-16 text-2xl"
-                onClick={goGame}
-            >
-                매칭하기
-            </button>
+        <div>
+            {login.loginInfo.isLogin ? (
+                <div className="bg-myWhite h-screen flex justify-center items-center flex-col">
+                    <div className="w-1/4 aspect-square bg-gray-900 rounded-3xl">{login.loginInfo.id}</div>
+                    <button
+                        className="rounded-full active:bg-sky-600 bg-sky-500 hover:bg-sky-400 py-1 mt-5 w-1/5 h-16 text-2xl"
+                        onClick={goGame}
+                    >
+                        매칭하기
+                    </button>
+                </div>
+            ) : (
+                <div></div>
+            )}
         </div>
     );
 }
