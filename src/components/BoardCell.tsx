9eb0ffc1888b14b16191afca0690ca9217acbbcf
angrymusic <angrymusic@naver.com>
2023-08-26 00:07:11 +0900

    castle 이미지 적용

--- src/components/BoardCell.tsx
+++ src/components/BoardCell.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import myCastle from "../images/my-castle.svg";
+import enemyCastle from "../images/enemy-castle.svg";
 
 interface BoardCellProps {
-    player: string;
-    onClick: () => void;
+  player: string;
+  onClick: () => void;
 }
 
 const BoardCell: React.FC<BoardCellProps> = ({ player, onClick }) => {
-    return (
-        <div className="border w-full h-full border-gray-400 flex items-center justify-center aspect-square" onClick={onClick}>
-            {player}
-        </div>
-    );
+  return (
+    <div
+      className="border w-full h-full border-gray-400 flex items-center justify-center aspect-square"
+      onClick={onClick}
+    >
+      {player === "ME" && <img src={myCastle} alt="my castle" />}
+      {player === "YOU" && <img src={enemyCastle} alt="enemy castle" />}
+    </div>
+  );
 };
 
 export default BoardCell;
